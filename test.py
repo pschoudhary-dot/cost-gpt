dbfe08e17ea6ed4deee3793cf6e8c300383dec04
Prashant Choudhary <pschoudhary.enacton@gmail.com>
2025-02-03 10:07:13 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 import os
  
-genai.configure(api_key="AIzaSyCJyw6yG29DS6D0gUcIK2ah3Ji14rVMCS4")
+genai.configure(api_key="yourapi")
  
 models = genai.list_models()
 for model in models:
